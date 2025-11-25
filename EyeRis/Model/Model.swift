df342e3973c999b78eff005b14de6f097107dd5d
adityaraj-143 <adityaxraj@gmail.com>
2025-11-25 17:22:03 +0530

    Added models for Acuity test, blink rate test, user, and changesd the format of instruction fetching, and more

--- EyeRis/Model/Model.swift
+++ EyeRis/Model/Model.swift
@@ -5,10 +5,20 @@
 //  Created by SDC-USER on 24/11/25.
 //
 
-struct ExerciseInstruction{
-    var title: String
-    var description: [String]
-    var video: String
+import Foundation
+
+struct User{
+    var firstName: String
+    var lastName: String
+    var gender: String
+    var dob: Date
+    var eyeHealthData: UserEyeHealthData
+}
+
+struct UserEyeHealthData{
+    // Store all the onboarding data
+    // also extra data if needed
+    var condition: [Conditions] // think about this, if this has to be here on in userdata
 }
 
 struct Exercise{
@@ -17,6 +27,27 @@ struct Exercise{
     var duration: Int
     var instructions: ExerciseInstruction
     var condition : [Conditions]
+}
+
+struct ExerciseInstruction{
+    var title: String
+    var description: [String]
+    var video: String
+}
+
+// to get the comment for either test or exercise based on the details passed as params
+struct Comment {
+    var exerciseComments: [String]
+    var testComments: [String]
+    
+    func getComment(accuracy: Int, speed: Int) -> String{
+        
+        return exerciseComments[Int.random(in: 0..<exerciseComments.count)]
+    }
+    
+    func getComment(testScore: [Int]) -> String {
+        return testComments[Int.random(in: 0..<testComments.count)]
+    }
 }
 
 enum Conditions{ // more conditions to be added, research needed
@@ -30,3 +61,39 @@ struct EyeTips{
     var tip: String
     var condition: [Conditions]
 }
+
+struct PerformedExerciseStat {
+    var id: Int
+    var name: String
+    var performedOn: Date
+    var accuracy: Int
+    var speed: Int
+}
+
+struct AcuityScore{ // need to change this (research needed)
+    var temp: String
+    
+    func calcScore () {
+        // should return the score of the test
+    }
+}
+
+struct AcuityTest{
+    // put the details for the snellen chart, and respective score details in the score struct
+//    var id: Int
+//    var leftEyeScore: AcuityScore
+}
+
+struct BlinkRateTest{
+    var instructions: TestInstruction
+    var passages: [String]
+    var duration: Int = 120
+}
+
+struct TestInstruction{
+//    var title: String
+    var description: [String]
+    var images: [String]
+}
+
+
