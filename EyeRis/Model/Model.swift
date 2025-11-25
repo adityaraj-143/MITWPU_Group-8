e34309a55bf14cd6453677841404cf95df492c4d
adityaraj-143 <adityaxraj@gmail.com>
2025-11-25 14:34:39 +0530

    added few models - Test, Exercise

--- EyeRis/Model/Model.swift
+++ EyeRis/Model/Model.swift
@@ -5,6 +5,23 @@
 //  Created by SDC-USER on 24/11/25.
 //
 
-struct Test {
-    var abc: String
+struct ExerciseInstruction{
+    var title: String
+    var description: [String]
+    var video: String
+}
+
+struct Exercise{
+    var id: Int
+    var name: String
+    var duration: Int
+    var instructions: ExerciseInstruction
+    var condition : [Conditions]
+}
+
+enum Conditions{ // more conditions to be added, research needed
+    case dryEyes
+    case eyeFatigue
+    case blurredVision
+    case wateryEyes
 }
