a7867e1bb0e6663646220692b1ac3ecf3aae67f3
Akhilesh-Mykalwar <akhileshmykalwar633@gmail.com>
2025-12-08 17:14:56 +0530

    WE DID IT BOIS.

--- EyeRis/SceneDelegate.swift
+++ EyeRis/SceneDelegate.swift
@@ -16,19 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-//        guard let windowScene = (scene as? UIWindowScene) else { return }
-//
-//        let window = UIWindow(windowScene: windowScene)
-//
-//        // Load ChatBot storyboard
-//        let storyboard = UIStoryboard(name: "TestResultFlow", bundle: nil)
-//        let initialVC = storyboard.instantiateInitialViewController()!
-//
-//        // Set root controller
-//        window.rootViewController = initialVC
-//        self.window = window
-//        window.makeKeyAndVisible()
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+
+        // Load ChatBot storyboard
+        let storyboard = UIStoryboard(name: "ChatBot", bundle: nil)
+        let initialVC = storyboard.instantiateInitialViewController()!
+
+        // Set root controller
+        window.rootViewController = initialVC
+        self.window = window
+        window.makeKeyAndVisible()
+
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
