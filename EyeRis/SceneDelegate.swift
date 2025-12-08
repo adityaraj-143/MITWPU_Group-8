8046618bad363d07dd968591535892f0dc012105
harshmalu9 <hmalu975@gmail.com>
2025-12-08 16:36:12 +0530

    Created the layout for the main test history page, no connections, actions and outlets added yet.

--- EyeRis/SceneDelegate.swift
+++ EyeRis/SceneDelegate.swift
@@ -17,7 +17,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-    }
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//
+//        let window = UIWindow(windowScene: windowScene)
+//
+//        // Load ChatBot storyboard
+//        let storyboard = UIStoryboard(name: "TestResultFlow", bundle: nil)
+//        let initialVC = storyboard.instantiateInitialViewController()!
+//
+//        // Set root controller
+//        window.rootViewController = initialVC
+//        self.window = window
+//        window.makeKeyAndVisible()
+//    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
---
+++ EyeRis/TestResultFlowViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TestResultFlowViewController.swift
+//  EyeRis
+//
+//  Created by SDC-USER on 03/12/25.
+//
+
+import UIKit
+
+class TestResultFlowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
