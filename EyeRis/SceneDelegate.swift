f6aa16494205d624c7ba8df16c0d605339647a77
harshmalu9 <hmalu975@gmail.com>
2025-12-08 17:47:03 +0530

    Added the modals for calendar and info, modals not designed yet, cross button not working in info modal.

--- EyeRis/SceneDelegate.swift
+++ EyeRis/SceneDelegate.swift
@@ -29,7 +29,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        window.rootViewController = initialVC
 //        self.window = window
 //        window.makeKeyAndVisible()
-//    }
+    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
