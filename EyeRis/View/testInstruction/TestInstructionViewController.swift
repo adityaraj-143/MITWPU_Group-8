89268a1e504f325568c88c6d47cd0496849dea2d
adityaraj-143 <adityaxraj@gmail.com>
2025-12-15 11:06:55 +0530

    completed test instructions page added the navigation from homescreen to instruction page

--- EyeRis/View/testInstruction/TestInstructionViewController.swift
+++ EyeRis/View/testInstruction/TestInstructionViewController.swift
@@ -1,104 +1,144 @@
-//
-//  TestInstructionViewController.swift
-//  EyeRis
-//
-//  Created by SDC-USER on 11/12/25.
-//
+    //
+    //  TestInstructionViewController.swift
+    //  EyeRis
+    //
+    //  Created by SDC-USER on 11/12/25.
+    //
 
-import UIKit
+    import UIKit
 
-class TestInstructionViewController: UIViewController, UICollectionViewDelegate {
+    class TestInstructionViewController: UIViewController, UICollectionViewDelegate {
 
 
-    @IBOutlet weak var instructionLabel: UILabel!
-    @IBOutlet weak var CollectionView: UICollectionView!
-    
-    let test = mockTest
+        @IBOutlet weak var pageControlOutlet: UIPageControl!
+        @IBOutlet weak var instructionLabel: UILabel!
+        @IBOutlet weak var CollectionView: UICollectionView!
+        
+        let test = mockTest
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        override func viewDidLoad() {
+            super.viewDidLoad()
 
-        registerCell()
+            registerCell()
 
-        CollectionView.dataSource = self
-        CollectionView.delegate = self
-        CollectionView.showsHorizontalScrollIndicator = false
-        CollectionView.collectionViewLayout = generateLayout()
+            CollectionView.dataSource = self
+            CollectionView.delegate = self
+            CollectionView.showsHorizontalScrollIndicator = false
+            CollectionView.collectionViewLayout = generateLayout()
 
-        instructionLabel.text = test.instruction.description.first
-    }
-
-    private func registerCell() {
-        CollectionView.register(
-            UINib(nibName: "TestInstructionsCollectionViewCell", bundle: nil),
-            forCellWithReuseIdentifier: "TestInstructionsCollectionViewCell"
-        )
-    }
-}
-
-// MARK: - Collection View Layout
-extension TestInstructionViewController {
-
-    func generateLayout() -> UICollectionViewLayout {
-
-        let layout = UICollectionViewCompositionalLayout { section, env in
-
-            let itemSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(1.0),
-                heightDimension: .fractionalHeight(1.0)
-            )
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-
-            let groupSize = NSCollectionLayoutSize(
-                widthDimension: .fractionalWidth(0.8),   // slightly smaller to show next page peeking
-                heightDimension: .fractionalHeight(1.0)
-            )
-            let group = NSCollectionLayoutGroup.horizontal(
-                layoutSize: groupSize,
-                subitems: [item]
-            )
-
-            group.interItemSpacing = .fixed(10)
-
-            let section = NSCollectionLayoutSection(group: group)
-            section.orthogonalScrollingBehavior = .groupPagingCentered
-            section.interGroupSpacing = 10
+            instructionLabel.text = test.instruction.description.first
+            pageControlOutlet.numberOfPages = test.instruction.description.count
+            pageControlOutlet.currentPage = 0
             
-            return section
+            pageControlOutlet.addTarget(
+                self,
+                action: #selector(pageControlChanged(_:)),
+                for: .valueChanged
+            )
+
+
         }
 
-        return layout
-    }
-}
-
-// MARK: - DataSource
-extension TestInstructionViewController: UICollectionViewDataSource {
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return test.instruction.description.count
+        private func registerCell() {
+            CollectionView.register(
+                UINib(nibName: "TestInstructionsCollectionViewCell", bundle: nil),
+                forCellWithReuseIdentifier: "TestInstructionsCollectionViewCell"
+            )
+        }
     }
 
-    func collectionView(_ collectionView: UICollectionView,
-                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    // MARK: - Collection View Layout
+    extension TestInstructionViewController {
 
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: "TestInstructionsCollectionViewCell",
-            for: indexPath
-        ) as! TestInstructionsCollectionViewCell
+        func generateLayout() -> UICollectionViewLayout {
 
-        let img = test.instruction.images[indexPath.item]
-        cell.configureCell(image: img)
-        instructionLabel.text = "\(test.instruction.description[indexPath.item])"
+            let layout = UICollectionViewCompositionalLayout { section, env in
 
-        return cell
+                let itemSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(1.0),
+                    heightDimension: .fractionalHeight(1.0)
+                )
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+                let groupSize = NSCollectionLayoutSize(
+                    widthDimension: .fractionalWidth(0.8),   // slightly smaller to show next page peeking
+                    heightDimension: .fractionalHeight(1.0)
+                )
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: groupSize,
+                    subitems: [item]
+                )
+
+                group.interItemSpacing = .fixed(10)
+
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                section.interGroupSpacing = 10
+                
+                section.visibleItemsInvalidationHandler = { [weak self] items, offset, env in
+                    guard let self else { return }
+
+                    let centerX = offset.x + env.container.contentSize.width / 2
+
+                    if let centeredItem = items.min(by: {
+                        abs($0.frame.midX - centerX) < abs($1.frame.midX - centerX)
+                    }) {
+
+                        let index = centeredItem.indexPath.item
+
+                        // Label update
+                        self.instructionLabel.text = self.test.instruction.description[index]
+
+                        // PageControl update (animated)
+                        if self.pageControlOutlet.currentPage != index {
+                            UIView.animate(withDuration: 0.25) {
+                                self.pageControlOutlet.currentPage = index
+                            }
+                        }
+                    }
+                }
+
+
+                
+                return section
+            }
+
+            return layout
+        }
     }
-}
 
-// MARK: - Scroll Handling (update label)
-extension TestInstructionViewController: UIScrollViewDelegate {
+    // MARK: - DataSource
+    extension TestInstructionViewController: UICollectionViewDataSource {
 
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let page = Int(CollectionView.contentOffset.x / CollectionView.frame.size.width)
-        instructionLabel.text = test.instruction.description[page]
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return test.instruction.description.count
+        }
+
+        func collectionView(_ collectionView: UICollectionView,
+                            cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "TestInstructionsCollectionViewCell",
+                for: indexPath
+            ) as! TestInstructionsCollectionViewCell
+
+            let img = test.instruction.images[indexPath.item]
+            cell.configureCell(image: img)
+        
+
+            return cell
+        }
+        
+        @objc private func pageControlChanged(_ sender: UIPageControl) {
+            let index = sender.currentPage
+            let indexPath = IndexPath(item: index, section: 0)
+
+            CollectionView.scrollToItem(
+                at: indexPath,
+                at: .centeredHorizontally,
+                animated: true
+            )
+        }
+
     }
-}
+
