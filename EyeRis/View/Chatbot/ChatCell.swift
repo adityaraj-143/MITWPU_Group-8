11ae3370e8230ff2ae7d076af5b01fc44a0ca124
Akhilesh-Mykalwar <akhileshmykalwar633@gmail.com>
2025-12-16 18:16:25 +0530

    Workie Wokrje

---
+++ EyeRis/View/Chatbot/ChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatCell.swift
+//  EyeRis
+//
+//  Created by SDC-USER on 16/12/25.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
