a19e260d15ff65b60ce7bd9de190a21bcc1fa82c
harshmalu9 <hmalu975@gmail.com>
2026-01-14 12:20:19 +0530

    Added the files, methods and models for today's exercise set

---
+++ EyeRis/Controller/Today's Exercise Set/TodaysExerciseSetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodaysExerciseSetViewController.swift
+//  EyeRis
+//
+//  Created by SDC-USER on 14/01/26.
+//
+
+import UIKit
+
+class TodaysExerciseSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
