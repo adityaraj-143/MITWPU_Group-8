19a346f0d07db2bc50067a6fbaaa28e2ace96ed8
Akhilesh-Mykalwar <akhileshmykalwar633@gmail.com>
2026-02-10 11:43:15 +0530

    started with work mode card.

---
+++ EyeRis/View/Homepage/WorkModeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WorkModeCollectionViewCell.swift
+//  EyeRis
+//
+//  Created by SDC-USER on 10/02/26.
+//
+
+import UIKit
+
+class WorkModeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
