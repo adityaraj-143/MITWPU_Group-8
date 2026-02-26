af4ffdcabc3bb7706ee83229e41dc52eec497f83
adityaraj-143 <adityaxraj72004@gmail.com>
2026-02-26 11:33:36 +0530

    today'sexercise set card cell ui changes

--- EyeRis/View/TodaysExerciseSet/TodaysExerciseSetCollectionViewCell.swift
+++ EyeRis/View/TodaysExerciseSet/TodaysExerciseSetCollectionViewCell.swift
@@ -71,7 +71,7 @@ class TodaysExerciseSetCollectionViewCell: UICollectionViewCell {
             checkmark.tintColor = .lightGreen
         } else {
             checkmark.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
-            checkmark.tintColor = .lightGray
+            checkmark.layer.opacity = 0¸
         }
     }
 
