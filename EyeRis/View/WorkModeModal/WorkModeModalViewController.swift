22c77f5767d5774249d8e624244fb4df32025962
Akhilesh-Mykalwar <akhileshmykalwar633@gmail.com>
2026-02-27 16:32:31 +0530

    Created WorkModeTimeManager for global time calculation.

--- EyeRis/View/WorkModeModal/WorkModeModalViewController.swift
+++ EyeRis/View/WorkModeModal/WorkModeModalViewController.swift
@@ -1,111 +1,78 @@
 import UIKit
 
-class WorkModeModalViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    @IBOutlet weak var timerLabel: UILabel!
+class WorkModeModalViewController: UIViewController,
+                                    UIPickerViewDelegate,
+                                    UIPickerViewDataSource {
+
     @IBOutlet weak var picker: UIPickerView!
-    
-    private var timer: Timer?
-    private var remainingSeconds: Int = 0
-    private var endTime: Date?
-    private var isBreakCycle = false
-    private var initialDurationSeconds = 0
-    
+    @IBOutlet weak var timerLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         picker.delegate = self
         picker.dataSource = self
+
+        let savedMinutes = UserDefaults.standard.integer(forKey: "workModeMinutes")
+        picker.selectRow(savedMinutes, inComponent: 0, animated: false)
+
         timerLabel.isHidden = true
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleTick(_:)),
+            name: .workModeTick,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleStateChange(_:)),
+            name: .workModeStateChanged,
+            object: nil
+        )
     }
 
-    @IBAction func startTimerPressed(_ sender: UIButton) {
-        let minutes = picker.selectedRow(inComponent: 0)
-        guard minutes > 0 else { return }
-        
-        initialDurationSeconds = minutes * 60
-        startWorkCycle()
-    }
+    @objc private func handleStateChange(_ notification: Notification) {
+        guard let isRunning = notification.object as? Bool else { return }
 
-    private func startWorkCycle() {
-        isBreakCycle = false
-        remainingSeconds = initialDurationSeconds
-        endTime = Date().addingTimeInterval(TimeInterval(remainingSeconds))
-        
-        updateTimerLabel()
-        timerLabel.isHidden = false
-        picker.isHidden = true
-        
-        startTicking()
-    }
-
-    private func startBreakCycle() {
-        isBreakCycle = true
-        remainingSeconds = 20
-        endTime = Date().addingTimeInterval(20)
-        
-        timerLabel.text = "Time’s up.\nLook away for 20 secs."
-        timerLabel.isHidden = false
-        picker.isHidden = true
-        
-        startTicking()
-    }
-
-    private func startTicking() {
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(timeInterval: 1,
-                                     target: self,
-                                     selector: #selector(tick),
-                                     userInfo: nil,
-                                     repeats: true)
-    }
-
-    @objc private func tick() {
-        guard let endTime = endTime else { return }
-        let secondsLeft = Int(ceil(endTime.timeIntervalSinceNow))
-
-        if secondsLeft <= 0 {
-            fireCompletionHaptics()
-
-            if isBreakCycle {
-                dismiss(animated: true) // or reset if you prefer
-            } else {
-                startBreakCycle()
-            }
-            return
-        }
-
-        remainingSeconds = secondsLeft
-        updateTimerLabel()
-    }
-
-    private func updateTimerLabel() {
-        if isBreakCycle {
-            timerLabel.text = "Look away\n\(remainingSeconds)s"
+        if isRunning {
+            picker.isHidden = true
+            timerLabel.isHidden = false
         } else {
-            let minutes = remainingSeconds / 60
-            let seconds = remainingSeconds % 60
-            timerLabel.text = String(format: "%02d:%02d", minutes, seconds)
+            picker.isHidden = false
+            timerLabel.isHidden = true
         }
     }
 
-    private func fireCompletionHaptics() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.prepare()
-        generator.notificationOccurred(.success)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
-            generator.notificationOccurred(.success)
-        }
+    @objc private func handleTick(_ notification: Notification) {
+        guard let seconds = notification.object as? Int else { return }
+
+        let min = seconds / 60
+        let sec = seconds % 60
+        timerLabel.text = String(format: "%02d:%02d", min, sec)
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        timer?.invalidate()
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
-    // MARK: - PickerView
-    func numberOfComponents(in pickerView: UIPickerView) -> Int { return 1 }
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int { return 90 }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "\(row) min"
+    // MARK: Picker
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
+
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int { 90 }
+
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        "\(row) min"
+    }
+
+    func pickerView(_ pickerView: UIPickerView,
+                    didSelectRow row: Int,
+                    inComponent component: Int) {
+        UserDefaults.standard.set(row, forKey: "workModeMinutes")
     }
 }
