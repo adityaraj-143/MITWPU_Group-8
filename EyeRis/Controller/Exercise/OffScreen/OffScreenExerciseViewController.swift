36b741ac7ae8b95648a124981cf4ac184428e61e
adityaraj-143 <adityaxraj72004@gmail.com>
2026-02-27 16:00:17 +0530

    added dark mode suppporting bg colors and text colors, fixed nil error for icons

---
+++ EyeRis/Controller/Exercise/OffScreen/OffScreenExerciseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OffScreenViewViewController.swift
+//  EyeRis
+//
+//  Created by SDC-USER on 27/02/26.
+//
+
+import UIKit
+
+class OffScreenExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
