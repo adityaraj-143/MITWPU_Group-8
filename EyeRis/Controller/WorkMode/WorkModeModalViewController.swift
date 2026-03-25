e95a718e18c1269b2826b2bf15b10ccbc7add7fe
Akhilesh-Mykalwar <akhileshmykalwar633@gmail.com>
2026-03-25 15:06:21 +0530

    Changed the timer setting(Previously changed for testing)

--- EyeRis/Controller/WorkMode/WorkModeModalViewController.swift
+++ EyeRis/Controller/WorkMode/WorkModeModalViewController.swift
@@ -7,7 +7,7 @@ class WorkModeModalViewController: UIViewController,
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet weak var timerLabel: UILabel!
     
-    private let minuteOptions = Array(stride(from: 1, through: 1, by: 10))
+    private let minuteOptions = Array(stride(from: 20, through: 50, by: 5))
     override func viewDidLoad() {
         super.viewDidLoad()
         
